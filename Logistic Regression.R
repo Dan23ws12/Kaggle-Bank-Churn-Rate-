a818839e19bdc05bfb3c603a208467f8890c1042
Dan23ws12 <danieloyeleye247@gmail.com>
2024-04-13 22:55:49 -0400

    made predictiond for Kaggle website, results were good, in 'Logistic Regression.R' I normalized the data before seperating the X and Y variables

--- Logistic Regression.R
+++ Logistic Regression.R
@@ -11,12 +11,15 @@ setwd("C:/Users/danie/Documents/data projects/Bank Churn Rate/Test and Submissio
 bank_churn <- read_excel("train.xlsx")
 
 #turning gender, geography, has credit card, is active member and exited to categorical variables
-bank_churn$Gender <- factor(bank_churn$Gender)
-bank_churn$Geography <- factor(bank_churn$Geography)
-bank_churn$HasCrCard <- factor(bank_churn$HasCrCard, levels = c(0, 1), labels = c("no", "yes"))
-bank_churn$IsActiveMember <- factor(bank_churn$IsActiveMember, levels = c(0, 1), labels = c("no", "yes"))
 bank_churn$Exited <- factor(bank_churn$Exited, levels = c(0, 1), labels = c("no", "yes"))
-
+categorize_vars <- function(df){
+  df$Gender <- factor(df$Gender)
+  df$Geography <- factor(df$Geography)
+  df$HasCrCard <- factor(df$HasCrCard, levels = c(0, 1), labels = c("no", "yes"))
+  df$IsActiveMember <- factor(df$IsActiveMember, levels = c(0, 1), labels = c("no", "yes"))
+  return(df)
+}
+bank_churn <- categorize_vars(bank_churn)
 #printing the summary
 summary(bank_churn)
 
@@ -24,20 +27,20 @@ summary(bank_churn)
 train_data <- bank_churn %>% sample_frac(0.7)
 test_data <- anti_join(bank_churn, train_data)
 
-#seperating explanatory and response variables 
-x_train_data <- select(train_data, - Exited)
-y_train_data <- train_data$Exited
-x_train_encoded <- model.matrix(~., x_train_data) #to make the x for glmnet
-y_test_data <- test_data$Exited
-x_test_data <- test_data %>% select(-Exited)
-
 #scaling test data with respect to training data
-test_transform <- preProcess(train_data)
-test_data <- predict(test_transform, test_data)
+model_test_transform <- preProcess(train_data)
+test_data <- predict(model_test_transform, test_data)
 #scaling training data
 train_data <- train_data %>% mutate_if(is.numeric, ~(scale(.) %>% as.vector))
 
-#run logistic regressions and find the threshold that gives max Area Uder the Curve and minimal
+#seperating explanatory and response variables 
+x_train_data <- select(train_data, - Exited)
+y_train_data <- train_data$Exited
+x_train_encoded <- model.matrix(~., x_train_data) #one-hot encoding to make the x for glmnet
+y_test_data <- test_data$Exited
+x_test_data <- test_data %>% select(-Exited)
+
+#run logistic regressions and find the threshold that gives max Area Under the Curve and minimal
 # classification error
 log_auc <- cv.glmnet(x_train_encoded, y_train_data, family = "binomial", type.measure = "auc")
 log_misclassify <- cv.glmnet(x_train_encoded, y_train_data, family = "binomial", type.measure = "class")
@@ -49,3 +52,26 @@ plot(log_auc)
 log_lm2 <- glm2(Exited ~., data = train_data, family = "binomial")
 pred <- predict(log_lm2, x_test_data, type = "response")
 ggroc(roc(y_test_data, pred))
+
+#creating results for Kaggle prediction
+
+#Read in data
+pred_data <- read.csv("test.csv")
+
+#set aside id
+pred_id <- pred_data$id
+
+#remove unnecessary columns like id and customerId
+pred_data <- pred_data %>% select(-c(CustomerId, Surname, id))
+
+#made HasCreditCard, Geography etc into categorical variables
+pred_data <- categorize_vars(pred_data)
+
+#normalized numeric data
+pred_data <- predict(model_test_transform, pred_data)
+
+#predictions (type argument is "class" for returning the prediction of Exited or not)
+final_pred<- predict(log_misclassify, newx = model.matrix(~., pred_data), s = "lambda.min", type = "response")
+final_pred <- as.vector(final_pred)
+final_pred <- data.frame(id = pred_id, Exited = final_pred)
+write.csv(final_pred, "log_predictions.csv", row.names = FALSE)
--- Random Forest Classifier.R
+++ Random Forest Classifier.R
@@ -8,31 +8,51 @@ set.seed(73)
 setwd("C:/Users/danie/Documents/data projects/Bank Churn Rate/Test and Submission Data")
 bank_churn <- read_excel("train.xlsx")
 #turning gender, geography, has credit card, is active member and exited to categorical variables
-bank_churn$Gender <- factor(bank_churn$Gender)
-bank_churn$Geography <- factor(bank_churn$Geography)
-bank_churn$HasCrCard <- factor(bank_churn$HasCrCard, levels = c(0, 1), labels = c("no", "yes"))
-bank_churn$IsActiveMember <- factor(bank_churn$IsActiveMember, levels = c(0, 1), labels = c("no", "yes"))
+categorize_vars <- function(df){
+  df$Gender <- factor(df$Gender)
+  df$Geography <- factor(df$Geography)
+  df$HasCrCard <- factor(df$HasCrCard, levels = c(0, 1), labels = c("no", "yes"))
+  df$IsActiveMember <- factor(df$IsActiveMember, levels = c(0, 1), labels = c("no", "yes"))
+  return(df)
+}
+bank_churn <- categorize_vars(bank_churn)
 bank_churn$Exited <- factor(bank_churn$Exited, levels = c(0, 1), labels = c("no", "yes"))
 #printing the summary
 summary(bank_churn)
 #separating data into training and test data
-train <- bank_churn %>% sample_frac(0.7)
-test <- anti_join(bank_churn, train)
-y_test <- test$Exited
-x_test <- test %>% select(-Exited)
+train_data <- bank_churn %>% sample_frac(0.7)
+test_data <- anti_join(bank_churn, train_data)
+y_test <- test_data$Exited
+x_test <- test_data %>% select(-Exited)
 #normalizing test data
-x_test$CreditScore = (x_test$CreditScore - mean(train$CreditScore))/sd(train$CreditScore)
-x_test$Age = (x_test$Age - mean(train$Age))/sd(train$Age)
-x_test$Tenure = (x_test$Tenure - mean(train$Tenure))/sd(train$Tenure)
-x_test$Balance = (x_test$Balance - mean(train$Balance))/sd(train$Balance)
-x_test$NumOfProducts = (x_test$NumOfProducts - mean(train$NumOfProducts))/sd(train$NumOfProducts)
-x_test$EstimatedSalary = (x_test$EstimatedSalary - mean(train$EstimatedSalary))/sd(train$EstimatedSalary)
+model_test_transform <- preProcess(train_data)
+x_test <- predict(model_test_transform, x_test)
 #normalizing training data
-train <- train %>% mutate_if(is.numeric, ~(scale(.) %>% as.vector))
+train_data <- train_data %>% mutate_if(is.numeric, ~(scale(.) %>% as.vector))
 #run the Random Forest Model
-randFr <- randomForest(Exited ~ ., data = train, importance = TRUE)
+randFr <- randomForest(Exited ~ ., data = train_data, importance = TRUE)
 randFr
 # confusion matrix on test data
 confusionMatrix(y_test, predict(randFr, newdata = x_test))
 
+#competition prediction data
+pred_data <- read.csv("test.csv")
 
+#set aside id
+pred_id <- pred_data$id
+
+#remove unnecessary columns like id and customerId
+pred_data <- pred_data %>% select(-c(CustomerId, Surname, id))
+
+#made HasCreditCard, Geography etc into categorical variables
+pred_data <- categorize_vars(pred_data)
+
+#normalized numeric data
+pred_data <- predict(model_test_transform, pred_data)
+
+#predicting the probabilities of exiting using the prediction data
+final_pred <- as.data.frame(predict(randFr, newdata = pred_data, type = "prob")) 
+
+#exporting the dataframe made from the predictions to a csv file
+final_pred <- data.frame(id = pred_id, Exited = final_pred$yes)
+write.csv(final_pred, file = "random_forest_prediction.csv", row.names = FALSE)
